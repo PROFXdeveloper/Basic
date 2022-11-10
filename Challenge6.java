1545dec068b7e453f6b8b6e6091d15a30e9a62e9
Nick Nick <88535836+PROFXdeveloper@users.noreply.github.com>
2022-11-10 08:40:48 +0800

    Add files via upload

---
+++ Challenge6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Challenge6 {
+
+    public static void main(String[] Strings) {
+
+        int minutesinYear = 60*24*365;
+        
+
+        Scanner scan = new Scanner(System.in);
+    
+        System.out.print("Input the numbers of minutes: ");
+        int min = scan.nextInt();
+    
+        int years =  (min / minutesinYear);
+        int remainder = (365*years);
+        int days =  (min / 1440 ) - r;
+    
+        System.out.println( min + " minutes is approximately " + years + " years and " + days + " days");
+    
+         }
+    
+    }
